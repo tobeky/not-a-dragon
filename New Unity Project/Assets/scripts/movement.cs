5f62b5f322a94cfb9c58a81a13622c7263de4446
tobeky <tobylewkowicz@bennington.edu>
2015-09-14 20:35:18 -0400

    finishing up

--- New Unity Project/Assets/scripts/movement.cs
+++ New Unity Project/Assets/scripts/movement.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class movement : MonoBehaviour {
-	Rigidbody nyoom; //variable for sphere
-	int speed;
+	private Rigidbody rbody; //variable for sphere
+	public int speed; //makes ball speed changeable
+	private int score; //establishes score variable
+	public Text scoreshow; //creates visible score
+
 	// Use this for initialization
 	void Start () {
-		nyoom = GetComponent <Rigidbody> ();
-		speed = 3;
+		rbody = GetComponent <Rigidbody> ();
+
 	}
 	
 	// Update is called once per frame
@@ -15,7 +19,19 @@ public class movement : MonoBehaviour {
 		float xMovement = Input.GetAxis ("Horizontal");
 		float zMovement = Input.GetAxis ("Vertical");
 
-		nyoom.AddForce (new Vector3 (xMovement, 0, 0)*speed); //this makes ball move along x axis
-		nyoom.AddForce (new Vector3 (0, 0, zMovement)*speed); //this makes ball move along z axis
+		rbody.AddForce (new Vector3 (xMovement, 0, 0)*speed); //this makes ball move along x axis
+		rbody.AddForce (new Vector3 (0, 0, zMovement)*speed); //this makes ball move along z axis
+
+
 	}
+
+	void OnTriggerEnter (Collider trigger) {
+		score = score + 1; //increases score by one
+		scoreshow.text = "Your score is " + score; //displays score
+		trigger.gameObject.SetActive(false); //makes disappear when hit
+		if(score >= 4) {
+			scoreshow.text = "Win!";
+		}
+	}
+	
 }
